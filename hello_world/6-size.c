d12b4740903fb6a3b4923cc5da571141123094e5
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-10-12 14:05:16 -0700

    C program that prints the size of various types on the computer it is compiled and run on

---
+++ hello_world/6-size.c
@@ -0,0 +1,19 @@
+/*
+ * File: 6-size.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main -  "C program that prints the size of various
+ *                types on the computer it is compiled and run on.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char charType;
+	 printf("Size of char: %zu byte\n", sizeof(charType));
+
+	return (0);
+}
