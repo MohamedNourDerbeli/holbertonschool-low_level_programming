e2f37aeba14e1f18bb802cb3ba92bfea085c8d53
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-10-14 22:29:48 +0100

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -12,7 +12,7 @@
  */
 int main(void)
 {
-	ltrace("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 
 	return (1);
 }
