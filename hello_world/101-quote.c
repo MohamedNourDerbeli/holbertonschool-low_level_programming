0fabde01a484f4b70737964c6bfc842432662d94
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-10-14 22:17:58 +0100

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ hello_world/101-quote.c
@@ -0,0 +1,18 @@
+/*
+ * File: 101-quote.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "and that piece of art is useful"
+ *                - Dora Korpar, 2015-10-19
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+	getc("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (1);
+}
