ad4d68e23300c57455343b13dcee2e2ed209cdce
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-10-16 15:50:44 +0100

    program that prints the alphabet in lowercase, followed by a new line.

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -  "program that prints the alphabet
+ *              in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z\n'; ch++)
+putchar(ch); 
+return (0);
+}
