5533641f9022f29df5a35d7306fa090f87eaad31
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-10-17 09:21:50 +0100

    Write a program that prints all the numbers of base 16 in lowercase, followed by a new line.

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main block
+ * Description: Print all possible combinations of single-digit numbers.
+ * Numbers must be separated by commas and a space.
+ * You can only use `putchar` to print to the console.
+ * You can only use `putchar` up to four times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+	int t;
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+	}
+	for (t = 97; t < 102; t++)
+	{
+		putchar(t);
+	}
+	putchar('\n');
+	return (0);
+}
