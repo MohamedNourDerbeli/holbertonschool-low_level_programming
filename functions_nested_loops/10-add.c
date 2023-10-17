118df79e9a6416fa97b27cd80ed48e8ac7726748
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-10-17 18:36:17 +0100

    function that adds two integers and returns the result.

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * times_table - Check description
+ * Description: It prints 9 times table starting with 0
+ * Return: Nothing.
+ */
+int add(int s, int l)
+{
+	_putchar((s + l) + '0');
+}
