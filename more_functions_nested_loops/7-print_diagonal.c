b01ffa1cfbb4b4c01d5a391a580fba9134df9270
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-10-19 13:16:35 +0100

    a function that draws a diagonal line on the terminal.

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - Check description
+ * Description: function that checks for uppercase character.
+ * @n : integer
+ * Return: void (Success) .
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(92);
+	}
+	_putchar('\n');
+}
