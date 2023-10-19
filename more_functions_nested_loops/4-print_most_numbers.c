107558f3d06e6c241a7c2e5e90c0c4399cf9ac69
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-10-19 12:11:13 +0100

    function that prints the numbers, from 0 to 9, followed by a new line.

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - Check description
+ * Description: function that checks for uppercase character.
+ *
+ * Return: void (Success) .
+ */
+void print_most_numbers(void)
+{
+	int x;
+	for (x = 0; x < 10; x++)
+	{
+		if (x != 2 && x != 4)
+		{
+			_putchar(x + '0');
+		}
+	}
+	_putchar('\n');
+}
