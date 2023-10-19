e5249c86380535c65a05c0b82a19bf5efca4e0a0
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-10-19 11:55:54 +0100

    a function that prints the numbers, from 0 to 9, followed by a new line.

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isdigit - Check description
+ * Description: function that checks for uppercase character.
+ * Return: void (Success) .
+ */
+
+void print_numbers(void)
+{
+    int i;
+    
+    for ( i = 0; i < 10; i++)
+    {
+        _putchar(i);
+    }
+    
+}
