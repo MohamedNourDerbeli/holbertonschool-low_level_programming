b9c166ae068074eefd391225e0587cd32a6b7862
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-10-19 13:03:56 +0100

    Write a function that draws a straight line in the terminal.

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - Check description
+ * Description: function that checks for uppercase character.
+ * @n : integer
+ * Return: void (Success) .
+ */
+
+void print_line(int n)
+{
+    int i ;
+
+    for  (i = 0; i < n; n++)
+    {
+        _putchar(45);
+    }
+    _putchar ('\n');
+}
