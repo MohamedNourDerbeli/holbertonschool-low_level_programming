1f7f392e92210a7843124ace28ef46f1504d6347
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-10-27 21:49:25 +0100

    function that concatenates two strings

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * swap_int - check the code
+ * _strcat - check the code
  * @src : char
  * @dest : char
  * Return: dest.
@@ -12,7 +12,7 @@ char *_strcat(char *dest, char *src)
 {
 	int i = 0;
 	int len = strlen(dest);
-	
+
 	while (src[i] != '\0')
 	{
 		dest[len] = src[i];
