d19a6293f623335dc5780d550dba4bc458c17c50
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-01 09:56:19 +0100

    function that prints a string, followed by a new line.

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - Check description
+ * Description: function that checks for uppercase character.
+ *
+ * Return: void (Success) .
+ */
+void _puts_recursion(char *s)
+{
+    int i ;
+
+    while (s[i] != '\0')
+    {
+        return _puts_recursion(s[i] +1);
+    }
+    
+}
