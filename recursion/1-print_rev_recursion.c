428c865260ea0d4be9198b15cfa1ddb5c2aaf599
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-01 13:35:26 +0100

    function that returns the length of a string.

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>    
 /**
  * _print_rev_recursion - check the code
  * @s : char
@@ -9,11 +8,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	int len =strlen(s);
 	if (*s == '\0')
 	{
 		return;
 	}
-	putchar(len-1);
-	_print_rev_recursion(len-1);
+	_print_rev_recursion(s + 1);
+	putchar(*s);
 }
