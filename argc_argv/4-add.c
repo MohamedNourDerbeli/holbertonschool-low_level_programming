a0091b52cfd0afa97b3e425ea814166a32158f23
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-04 20:45:29 +0100

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * @argc: integer
+ * @argv: char
+ * Return: void (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		/* int result = 0;*/
+		int i;
+		int result = 0;
+		printf("%d\n", isdigit(*argv[1]));
+		for (i = 1; i < argc; i++)
+		{
+			int leng = strlen(argv[i]);
+			int j;
+			for (j = 0; j < leng; j++)
+			{
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+				/*printf("%d", argv[i][j] - '0'); */
+			}
+			result += atoi(argv[i]);
+
+			/*if (!isdigit(argv[i]))
+				{
+						printf("Error\n");
+						return (1);
+				}*/
+			/* result = result + atoi(argv[i]);*/
+		}
+		printf("%d\n", result);
+	}
+	return (0);
+}
