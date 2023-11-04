c6ff46e22e538b9b229417f2aa004eba8630743e
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-04 10:59:11 +0100

    4-pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -8,10 +8,13 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
+	if (y == 0)
 	{
 		return (-1);
 	}
-
+	if (y < 0)
+	{
+		return (_pow_recursion(x, y + 1) / x);
+	}
 	return (_pow_recursion(x, y - 1) * x);
 }
