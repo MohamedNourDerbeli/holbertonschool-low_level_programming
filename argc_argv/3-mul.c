70a3d417778bae4846da7189acb5c11d871fa6c6
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-04 18:07:17 +0100

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,6 +10,7 @@ int main(__attribute__((unused)) int argc, char *argv[])
 {
 	int s1 = atoi(argv[1]);
 	int s2 = atoi(argv[2]);
-	printf("%d", s1 * s2);
+
+	printf("%d\n", s1 * s2);
 	return (0);
 }
