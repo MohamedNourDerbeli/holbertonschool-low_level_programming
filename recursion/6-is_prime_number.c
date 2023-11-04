835c255d279ddf45e3fc39f9239fdd752a889868
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-04 12:21:15 +0100

    6-is_prime_number.c

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * sqrt2 - check the code
+ * @n :integer
+ * @guess : interger
+ * Return: Always 0.
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 1)
+	{
+		return (0);
+	}
+	if (n / 2 == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
