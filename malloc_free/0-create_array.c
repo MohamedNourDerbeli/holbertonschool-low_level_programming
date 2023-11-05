736c2e9627084dc8f20bad2b7473fa3c263d50d6
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-06 00:11:05 +0100

    0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,7 +11,11 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
 	char *cs;
-
+	
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	cs = malloc(sizeof(char) * size);
 
 	if (cs == NULL)
