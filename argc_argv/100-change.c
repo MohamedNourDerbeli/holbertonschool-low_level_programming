6a50d31e46f2b0abc3e3ea1c8c355d988f0e6e26
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-05 21:34:02 +0100

    100-change.c

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -8,38 +8,40 @@
  * @argv: char
  * Return: void (Success)
  */
+
 int main(int argc, char *argv[])
 {
-	int coins[] = {25, 10, 5, 2, 1};
-	 int cents = atoi(argv[1]);
+	int coins[] = {25, 10, 5, 2, 1}; 
+	int numCoins = sizeof(coins) / sizeof(coins[0]);
+	int coinCount = 0;
+	int i = 0;
+	int cents = atoi(argv[1]);
 
-	if (argc > 2 || argc == 1)
+	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		return 1;
 	}
+
 	if (cents < 0)
 	{
 		printf("0\n");
-		return (0);
+		return 0;
 	}
 
-    int numCoins = sizeof(coins) / sizeof(coins[0]);
+	while (cents > 0 && i < numCoins)
+	{
+		int currentCoin = coins[i];
+		if (cents >= currentCoin)
+		{
+			int numUsed = cents / currentCoin;
+			coinCount += numUsed;
+			cents -= numUsed * currentCoin;
+		}
+		i++;
+	}
 
-    int coinCount = 0;
-    int i = 0;
+	printf("%d\n", coinCount);
 
-    while (cents > 0 && i < numCoins) {
-        int currentCoin = coins[i];
-        if (cents >= currentCoin) {
-            int numUsed = cents / currentCoin;
-            coinCount += numUsed;
-            cents -= numUsed * currentCoin;
-        }
-        i++;
-    }
-
-    printf("%d\n", coinCount);
-
-    return 0;
+	return 0;
 }
