088f038408164b325e088013a4f881a37840e4cd
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-05 20:43:29 +0100

    100-change.c

---
+++ argc_argv/100-change.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * @argc: integer
+ * @argv: char
+ * Return: void (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
+	}
+	
+	if (atoi(argv[1]) % 25 ==0 )
+	{
+		printf("%d",atoi(argv[1]) / 25 );
+	}
+	else if (atoi(argv[1]) % 10 ==0 )
+	{
+		printf("%d\n",atoi(argv[1]) / 10 );
+	}
+	else if (atoi(argv[1]) % 5 ==0 )
+	{
+		printf("%d\n",atoi(argv[1]) / 5 );
+	}
+	else if (atoi(argv[1]) % 2 ==0 )
+	{
+		printf("%d\n",atoi(argv[1]) / 2 );
+	}
+	else if (atoi(argv[1]) % 1 ==0 )
+	{
+		printf("%d\n",atoi(argv[1]) / 1 );
+	}
+	return(0);
+}
