7ef63fbe4b732968ef84c7090505edd724deddad
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-06 17:04:06 +0100

    1-string_nconcat.c

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * string_nconcat - Entry point
+ * @s1 : char
+ * @s2 : char
+ * @n : int
+ * Return: void (Success)
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int len;
+	int len2;
+	char *temp;
+	char *con;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	len = strlen(s1);
+	len2 = strlen(s2);
+	if (n > len2)
+	{
+
+		con = malloc(len + len2 + 1);
+		temp = s2;
+	}
+	else
+	{
+		con = malloc(len + n + 1);
+		strncpy(temp, s2, n);
+	}
+
+	if (con == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(con, s1);
+	strcat(con, temp);
+	return (con);
+}
