292e55e7b70226d0f637771015e0e54550dcc538
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-06 19:52:55 +0100

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -6,20 +6,27 @@
  * _calloc - Entry point
  * @nmemb : char
  * @size : char
- * @n : int
  * Return: void (Success)
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *allc;
+	unsigned int i;
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	allc = (int *)calloc(nmemb, sizeof(int));
+	allc = malloc(nmemb * size);
 	if (allc == NULL)
 	{
 		return (NULL);
 	}
+	i = 0;
+	while (i < nmemb * size)
+	{
+		allc[i] = 0;
+		i++;
+	}
+
 	return (allc);
 }
