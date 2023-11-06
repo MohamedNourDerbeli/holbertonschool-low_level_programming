7e82d19962964a9f0a7502176d3f5ccc5f8b1fa9
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-06 09:02:49 +0100

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -18,12 +18,12 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	if (s1 != NULL && s2 == NULL)
+	if (s1 != NULL && s2 == '\0')
 	{
 		strcpy(con, s1);
 	}
 
-	if (s2 != NULL && s1 == NULL)
+	if (s2 != NULL && s1 == '\0')
 	{
 		strcat(con, s2);
 	}
