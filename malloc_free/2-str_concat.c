11e6eb47be13dcee05d6b33960082496bcfebf0e
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-06 08:53:28 +0100

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,12 +10,22 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	int i;
 	int len = (s1 != NULL) ? strlen(s1):0;
 	int len2 = (s1 != NULL) ? strlen(s2):0;
 
 	char *con = malloc(len + len2 + 1);
+	if (con == NULL)
+	{
+		return(NULL);
+	}
+	for ( i = 0; con[i]!= '\0'; i++)
+	{
+		printf("%d",con[i]);
+	}
 	
-	return(con[3]);
+	
+	return(0);
 	
 	
 }
