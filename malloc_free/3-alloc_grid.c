b2bbfd4ce153bcfd2f62d02e811d358c581f59a6
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-06 13:25:09 +0100

    3-alloc_grid.c

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -28,16 +28,19 @@ int **alloc_grid(int width, int height)
 		array[i] = malloc(sizeof(int *) * width);
 		if (array[i] == NULL)
 		{
-			for (--i; i >= 0; i--)
+			while (i >= 0)
 			{
 				free(array[i]);
+				i--;
 			}
 			free(array);
 			return (NULL);
 		}
 	}
 	for (i = 0; i < height; i++)
+	{
 		for (j = 0; j < width; j++)
 			array[i][j] = 0;
+	}
 	return (array);
 }
