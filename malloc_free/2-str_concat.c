ed40915d60906a11921cbf91f4f53febef31b047
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-06 01:01:07 +0100

    2-str_concat.c

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - Entry point
+ * @s1 : char
+ * @s2 : char
+ * Return: void (Success)
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0;
+	int len = strlen(s1);
+
+	if (s1 == NULL)
+	{
+		return (NULL);
+	}
+	while (s2[i] != '\0')
+	{
+		s1[len] = s2[i];
+		i++;
+		len++;
+	}
+	s1[len] = '\0';
+	int len2 = strlen(s1);
+	s1 = malloc(sizeof(char) * len2);
+	return (s1);
+}
