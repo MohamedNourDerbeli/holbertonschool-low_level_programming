a237f2be85404e7a972dd6dc8c9a7973d0990e8d
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-15 12:13:15 +0100

    free

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -13,22 +13,7 @@ void free_dog(dog_t *d)
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 	{
-		return (NULL);
-	}
-	l1 = strlen(d->name);
-	d->name = malloc(sizeof(char) * (l1 + 1));
-	if (d->name == NULL)
-	{
 		free(d);
 		return (NULL);
 	}
-	l2 = strlen(d->owner);
-	d->owner = malloc(sizeof(char) * (l2 + 1));
-	if (d->owner == NULL)
-	{
-		free(d->name);
-		free(d);
-		return (NULL);
-	}
-	return (d);
 }
