d7a42973eabb1a89d81b0f2af938370bb1c7f61c
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-15 12:22:17 +0100

    free

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,15 +1,13 @@
 #include "dog.h"
 /**
- * *new_dog - check the code
- * @name : char
- * @age : float
- * @owner : char
+ * free_dog - check the code
+ * @d : 
  * Return: Always 0.
  */
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
-		return (NULL);
+		return;
 	free(d->name);
 	free(d->owner);
 	free(d);
