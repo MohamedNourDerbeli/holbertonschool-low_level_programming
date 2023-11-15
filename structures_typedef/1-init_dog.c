92d041e485c6bcc357670cfe6d817702187c53d4
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-15 10:08:18 +0100

    int_dog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -7,10 +7,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
-	d->age = age;
-	d->name = name;
-	d->owner = owner;
+	(*d).age = age;
+	(*d).name = name;
+	(*d).owner = owner;
 
 	return (0);
 }
