9b12cf489b28eb95bedefb0e95dce8092cffbe94
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-15 10:12:05 +0100

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG
 #define DOG
+#include <stdio.h>
 
 /**
  * struct dog - check the code
@@ -15,6 +16,4 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 #endif
