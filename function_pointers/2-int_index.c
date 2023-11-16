9a14fb16c054df431a2804db4a4b69877d984d7d
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-16 10:42:51 +0100

    2-int

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -8,11 +8,7 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (*array != *cmp)
-	{
-		return(-1);
-	}
-	if (size <= 0)
+	if (array == NULL || size <= 0 || cmp == NULL)
 	{
 		return(-1);
 	}
