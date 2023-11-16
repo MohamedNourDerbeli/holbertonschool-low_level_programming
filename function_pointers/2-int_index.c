89f2bf598298fc6a330e2764a569d16a1521d1d4
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-16 10:31:05 +0100

    2-int

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - prints a name as is
+ * @array: name of the person
+ * @size : pointer
+ * @action : pointer
+ * Return: Nothing.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array == NULL)
+	{
+		return(-1);
+	}
+	if (size <= 0)
+	{
+		return(-1);
+	}
+	return((*cmp)(array));
+}
