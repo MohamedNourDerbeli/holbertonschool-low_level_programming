219173e7f93c12ce2154f5f2e349f0303594bfd4
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-18 00:42:45 +0100

    3-main

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	{
 		printf("Error\n"), exit(99);
 	}
-	if (!b && argv[2][0] == '/' || argv[2][0] == '%')
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		printf("Error\n"), exit(100);
 	}
