0064eeda4c2503f9061eb31ad0d63626af483124
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-17 21:36:08 +0100

    3-main

---
+++ function_pointers/3-main.c
@@ -0,0 +1,17 @@
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+	if (argc < 4 || argc > 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{
+		int a = atoi(argv[1]);
+		int b = atoi(argv[3]);
+		f(a,b);
+	}
+	
+}
