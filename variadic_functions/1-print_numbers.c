02ce18af182a2067aaf27c5bc4e610a98832364e
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-18 12:20:25 +0100

    print-numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -29,8 +29,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (i < n - 1)
 		{
 			printf("%s", separator);
-			printf("\n");
 		}
+		printf("\n");
 	}
 	va_end(list);
 }
