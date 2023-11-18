df33d47b5957d3c5f2926d9cc3077bdf2ec7036a
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-18 10:33:56 +0100

    op_fun

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,21 +1,48 @@
 #include "3-calc.h"
-
+/**
+ * op_add - check the code
+ * @a : int
+ * @b : int
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+/**
+ * op_sub - check the code
+ * @a : int
+ * @b : int
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+/**
+ * op_mul - check the code
+ *
+ * @a : int
+ * @b : int
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+ * op_div - check the code
+ *
+ * @a : int
+ * @b : int
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+/**
+ * op_mod - check the code
+ *
+ * @a : int
+ * @b : int
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
