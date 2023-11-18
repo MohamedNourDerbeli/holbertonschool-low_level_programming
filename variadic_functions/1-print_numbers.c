05a3f36d6bcd310f6e08cdbbdc5c96a6eb3d55ac
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-18 12:11:55 +0100

    print-numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - check the code
+ * @separator:car
+ * @n: int
+ * Return: Always 0.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i, num = 0;
+
+	if (n == 0)
+	{
+		return;
+	}
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(list, int);
+		printf("%d", num);
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+
+	va_end(list);
+}
