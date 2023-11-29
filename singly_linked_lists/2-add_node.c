34d03a7a77588b51cef6b2444235ff094390ed8e
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-29 15:36:39 +0100

    add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -8,8 +8,8 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
-	new_node->str =strdup(str);
-	new_node->len=strlen(new_node->str);
+	new_node->str = strdup(str);
+	new_node->len = strlen(new_node->str);
 	printf("[%d] %s\n", new_node->len, new_node->str);
 	new_node->next = *head;
 	return (0);
