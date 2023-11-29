158262c4f700fc4415b6a500a852ea7792d11534
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-29 11:43:14 +0100

    print list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * print_list - check the code
+ * @h: list
+ * Return: Always 0.
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *temp = h;
+	int s;
+	if (temp->str == NULL)
+	{
+		s = printf("[0] (nil)");
+	}
+
+	return (s);
+}
