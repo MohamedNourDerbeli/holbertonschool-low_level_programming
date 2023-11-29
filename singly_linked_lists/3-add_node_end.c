c2a5a653329252c0d4d3ac48efd57aea3735a4ed
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-29 16:56:36 +0100

    add node to the end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -9,8 +9,25 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 
 	list_t *new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	if (*head == NULL)
+	{
+		new_node->str = strdup(str);
+		if (new_node->str == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+
+		new_node->len = strlen(new_node->str);
 		*head = new_node;
+	}
+
 	else
 	{
 		list_t *lastNode = *head;
