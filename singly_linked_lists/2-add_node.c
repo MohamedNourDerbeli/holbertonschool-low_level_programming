2ebf51b15fe2c29d65728f47b7e6e68a7ae4326e
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-29 16:44:06 +0100

    add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -16,6 +16,11 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		return(NULL);
+	}
+	
 	new_node->len = strlen(new_node->str);
 	new_node->next = *head;
 	*head = new_node;
