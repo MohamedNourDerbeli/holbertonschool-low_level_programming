ca82adc01a2eee2c228c661e4807778c14ab4a8a
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-30 10:53:06 +0100

    print string

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * test - check the code
  *
  * Return: Always 0.
  */
-void main()
+void test()
 {
     printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 }
