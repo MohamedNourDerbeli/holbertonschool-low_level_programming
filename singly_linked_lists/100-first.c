37a502243fa9c531a6575fc6951ba9304cc59664
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-30 10:49:16 +0100

    print string

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -5,8 +5,7 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void main()
 {
     printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
 }
