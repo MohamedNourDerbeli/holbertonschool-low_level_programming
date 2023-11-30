83a690d56b0f16b7ad5899808742522d2709a175
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-30 10:54:49 +0100

    print string

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -7,5 +7,5 @@
  */
 void test()
 {
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 }
