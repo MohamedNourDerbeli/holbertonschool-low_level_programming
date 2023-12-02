c7ee7c8604a9d639d5dbac53e5392dbe23d63c99
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-02 11:36:38 +0100

    return sum of dlists

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,27 +1,24 @@
 #include "lists.h"
 /**
- * free_dlistint - check the code
+ * sum_dlistint - check the code
  * @head: list
- * Return: Always 0.
+ * Return: sum .
  */
 
 int sum_dlistint(dlistint_t *head)
 {
 	dlistint_t *sum;
 	int count = 0;
+
 	sum = head;
 	if (!sum)
-	{
 		return (0);
-	}
 	while (sum->next)
 	{
 		sum = sum->next;
 		count += sum->n;
 	}
 	if (count == 0)
-	{
 		return (0);
-	}
 	return (count);
 }
