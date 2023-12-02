96097a178a779aa9210644eead726adf82189293
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-02 10:00:25 +0100

    add new node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -9,19 +9,23 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+
+	if (!new_node || !head)
 	{
 		free(new_node);
 		return (NULL);
 	}
 	new_node->n = n;
-	if (new_node->n =='\0')
+	new_node->prev = NULL;
+	if (!*head)
 	{
-		free(new_node);
-		return (NULL);
+		*head = new_node;
+	 	new_node->next = NULL;
 	}
-
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
+	else
+	{
+		new_node = *head;
+		(*head)->prev = new_node;
+		*head = new_node;
+	}
 }
