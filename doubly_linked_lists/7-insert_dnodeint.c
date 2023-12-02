617bfd8eddfbbab8bac452b013c823f98426978f
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-02 17:14:41 +0100

    insert list

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -42,6 +42,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 		}
 	}
+	first_node->next->prev = node;
 	node->next = first_node->next;
 	first_node->next = node;
 	node->prev = first_node;
