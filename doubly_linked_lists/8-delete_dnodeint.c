d86d1a9e471cdf068f22012da1c04a334a9991af
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-02 21:47:09 +0100

    delete node

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -9,10 +9,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *selectedindex = *head;
 	unsigned int count = 0;
-
-	if (!*head)
-		return (-1);
-
+	
 	while (count != index)
 	{
 		selectedindex = selectedindex->next;
