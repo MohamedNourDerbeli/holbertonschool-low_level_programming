8a4c4df1b5579297fc2c4234c000f8f51aae579f
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-02 10:56:41 +0100

    add last node

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * add_node_end - check the code
+ * @head: list_t
+ * @str: char
+ * Return: Always 0.
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t)),*last_node;
+
+	if (!new_node || !head)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (!*head)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+	}
+	else
+	{
+		last_node = *head;
+		while (last_node->next)
+			last_node = last_node->next;
+		last_node->next = new_node;
+		new_node->prev = last_node;
+	}
+	return (new_node);
+}
