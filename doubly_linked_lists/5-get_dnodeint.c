fab6edf1be1c35581cedbb5b47bd5d858c15767b
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-02 11:23:02 +0100

    return the nth node

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - check the code
+ * @head: list_t
+ * @n: int
+ * Return: Always 0.
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *node;
+	unsigned int i = 0;
+	node = head;
+	if (!node)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < index; i++)
+	{
+		node = node->next;
+	}
+	return (node);
+}
