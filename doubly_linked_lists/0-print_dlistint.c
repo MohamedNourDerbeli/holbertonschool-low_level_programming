f21e52869dab4377dad7b4555aab3418ae59e560
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-02 08:00:00 +0100

    print list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * print_dlistint - check the code
+ * @h: pointer
+ * Return: size.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int size = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		size += 1;
+		h = h->next;
+	}
+	return (size);
+}
