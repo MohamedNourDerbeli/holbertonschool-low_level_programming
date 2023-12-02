94571195c6e8b52774f71b613708d3aeb88d395c
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-02 12:19:47 +0100

    insert list

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - check the code
+ * @h: list
+ * @n: int
+ * @idx: index of the list
+ * Return: Always 0.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *node = malloc(sizeof(dlistint_t)), *first_node = *h;
+	unsigned int count = 0;
+	if (!node || !idx || !n)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->n = n;
+
+	for (count = 0; count < idx; count++)
+	{
+		first_node = first_node->next;
+		if (node == NULL)
+		{
+			return (NULL);
+		}
+	}
+	node->next = first_node->next;
+	first_node->next = node;
+	node->prev = first_node;
+	return (node);
+}
