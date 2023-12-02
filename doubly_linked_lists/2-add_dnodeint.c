d8961a3a3bc7faedce712ad8b2c3a256253d418e
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-02 09:04:44 +0100

    add new node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -14,7 +14,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		free(new_node);
 		return (NULL);
 	}
-	new_node->n = n;
+	new_node->n = strdup(n);
+	if (new_node->n == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
