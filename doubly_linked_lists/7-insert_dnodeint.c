53c1ab60f698e464f8a1d874f4ce2015e3b9c234
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-02 13:50:02 +0100

    insert list

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -21,9 +21,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	for (count = 0; count <= idx; count++)
 	{
 		first_node = first_node->next;
-		if (sum->next == NULL)
+		if (node->next == NULL)
 		{
-			sum = sum->next;
+			node = node->next;
 			return (NULL);
 		}
 	}
