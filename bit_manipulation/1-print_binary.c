c3ecaa33f470cd79b79bc2037ae8336e99426986
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-03 11:34:38 +0100

    print binary

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -10,7 +10,7 @@ void print_binary(unsigned long int n)
 	int size = sizeof(n) * 8, count = 0;
 	while (size)
 	{
-		if (n && 1L << --size)
+		if (n & 1L << --size)
 		{
 			putchar('1');
 			count++;
