5204fb8bf77173c8e46a05a4d35306a28926d913
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-03 11:33:59 +0100

    print binary

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_binary - check the code
+ * @b: binary
+ * Return: Always 0.
+ */
+void print_binary(unsigned long int n)
+{
+	int size = sizeof(n) * 8, count = 0;
+	while (size)
+	{
+		if (n && 1L << --size)
+		{
+			putchar('1');
+			count++;
+		}
+	}
+	if (!count)
+	{
+		putchar('0');
+	}
+}
