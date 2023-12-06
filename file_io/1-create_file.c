099acc68a849d54f18bd844d26f7116e52027fe7
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-06 13:14:04 +0100

    create file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,20 @@
+
+#include "main.h"
+/**
+ * create_file - check the code
+ * @filename: char
+ * @text_content: text
+ * Return: Always 0.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd,n;
+	char buf[10000];
+
+	fd = open(filename, O_CREAT);
+	n = read(fd, buf, 600);
+	write(fd, buf, n);
+	close(fd);
+	return (1);
+}
