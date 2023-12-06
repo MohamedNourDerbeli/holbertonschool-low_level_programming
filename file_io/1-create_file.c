83e3bae118d63fd51e8a7617e6f70a1a4c674114
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-06 13:29:57 +0100

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,11 +9,11 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd,n;
+	int fd, n;
 	char buf[10000];
 
 	n = read(STDOUT_FILENO, buf, 600);
-	fd = open(filename, O_CREAT | O_WRONLY, text_content);
+	fd = open(filename, O_CREAT | O_RDWR, text_content);
 	if (fd == -1)
 		return (-1);
 	write(fd, buf, n);
