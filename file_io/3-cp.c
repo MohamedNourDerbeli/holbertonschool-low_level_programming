8428bedc73f7f476aa8839532557f7547ba8ecd0
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-06 17:10:31 +0100

    cp file_from file_to

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,11 +13,11 @@
  */
 ssize_t cp_textfile(const char *file_from, const char *file_to)
 {
-	int fd, fd1,x;
+	int fd, fd1, x;
 	char buf[1024];
 
 	fd = open(file_from, O_RDONLY);
-	x=read(fd, buf, 1000);
+	x = read(fd, buf, 1000);
 	close(fd);
 	fd1 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	write(fd1, buf, x);
@@ -26,13 +26,12 @@ ssize_t cp_textfile(const char *file_from, const char *file_to)
 }
 int main(int ac, char **av)
 {
-    int res;
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = cp_textfile(av[1], av[2]);
-    return (0);
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s filename text\n", av[0]);
+		exit(1);
+	}
+	cp_textfile(av[1], av[2]);
+	return (0);
 }
