5008dd1a715e29c70459d1e50ed95475fcbb0539
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-06 11:57:58 +0100

    read files

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -10,9 +10,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, n;
 	char buf[500];
-	
+
 	fd = open(filename, O_RDONLY);
 	n = read(fd, buf, letters);
 	write(1, buf, n);
+	close(fd);
 	return (n);
 }
