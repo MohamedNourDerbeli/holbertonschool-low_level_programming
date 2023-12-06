84b5d3e04ac17d579ddb1cd5854dcc50317b4449
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-06 11:56:00 +0100

    read files

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+/**
+ * read_textfile - check the code
+ * @filename: char
+ * @letters: size_t
+ * Return: Always 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, n;
+	char buf[500];
+	
+	fd = open(filename, O_RDONLY);
+	n = read(fd, buf, letters);
+	write(1, buf, n);
+	return (n);
+}
