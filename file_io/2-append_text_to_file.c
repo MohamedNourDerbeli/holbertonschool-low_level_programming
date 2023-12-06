6ec85aa879f234049decdcfa585cbd46603e2bd1
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-06 14:53:59 +0100

    append file

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+
+#include "main.h"
+/**
+ * create_file - check the code
+ * @filename: char
+ * @text_content: text
+ * Return: Always 0.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, n;
+
+	if (!text_content)
+		n = 0;
+	else
+		n = strlen(text_content);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	if (fd == -1)
+		return (-1);
+	write(fd, text_content, n);
+	close(fd);
+	return (1);
+}
