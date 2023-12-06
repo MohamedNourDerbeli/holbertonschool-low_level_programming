db29d0beb8ac069d412fecfacb71ae43a94aa02a
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-06 14:37:43 +0100

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,7 +12,10 @@ int create_file(const char *filename, char *text_content)
 	int fd, n = strlen(text_content);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
 	if (!text_content)
+	{
+		close(fd);
 		return (1);
+	}
 	if (fd == -1)
 		return (-1);
 	write(fd, text_content, n);
