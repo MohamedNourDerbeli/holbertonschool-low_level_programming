5d8fcf5a55864281698c61f30e2816e45ad21b79
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-06 17:05:06 +0100

    cp file_from file_to

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,5 +1,10 @@
-
-#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * read_textfile - check the code
  * @filename: char
@@ -8,13 +13,27 @@
  */
 ssize_t cp_textfile(const char *file_from, const char *file_to)
 {
-	int fd, fd1;
+	int fd, fd1,x;
 	char buf[1024];
+
 	fd = open(file_from, O_RDONLY);
-	read(fd, buf, 600);
+	x=read(fd, buf, 1000);
 	close(fd);
 	fd1 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	write(fd1, buf, 600);
+	write(fd1, buf, x);
 	close(fd1);
 	return (1);
+}
+int main(int ac, char **av)
+{
+    int res;
+
+    if (ac != 3)
+    {
+        dprintf(2, "Usage: %s filename text\n", av[0]);
+        exit(1);
+    }
+    res = cp_textfile(av[1], av[2]);
+    printf("-> %i)\n", res);
+    return (0);
 }
