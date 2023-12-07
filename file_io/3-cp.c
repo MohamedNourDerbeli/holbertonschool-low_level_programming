85df21bf05e52b3a35d96709f437463509531048
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-07 12:22:34 +0100

    cp file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #define ARGUMENT_NOT_CORRECT "Usage: cp file_from file_to\n"
 #define FILE_FROM_NOT_EXIST "Error: Can't read from file %s\n"
-#define FILE_TO_ERROR "Error: Can't write to %s_copy\n"
+#define FILE_TO_ERROR "Error: Can't write to %s\n"
 #define DESCRIPTOR_NOT_CLOSE "Error: Can't close fd %d\n"
 /**
  * read_textfile - check the code
@@ -20,7 +20,7 @@ ssize_t cp_textfile(const char *file_from, const char *file_to)
 		dprintf(1, FILE_FROM_NOT_EXIST, file_from), exit(98);
 	fd1 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd1 == -1)
-		dprintf(2, FILE_TO_ERROR, file_from), exit(99);
+		dprintf(2, FILE_TO_ERROR, file_to), exit(99);
 	while ((b = read(fd, buf, 1024)) > 0)
 		if (write(fd1, buf, b) != b)
 			dprintf(2, FILE_TO_ERROR, file_to), exit(99);
