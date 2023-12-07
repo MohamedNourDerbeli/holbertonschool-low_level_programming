6980c1a0b38f7c3238e1a9ee0725b7e53e71cc14
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-07 11:55:45 +0100

    cp file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,7 +7,7 @@
 #include <string.h>
 #define ARGUMENT_NOT_CORRECT "Usage: cp file_from file_to\n"
 #define FILE_FROM_NOT_EXIST "Error: Can't read from file %s\n"
-#define FILE_TO_ERROR "Error: Can't write to NAME_OF_THE_FILE\n"
+#define FILE_TO_ERROR "Error: Can't write to %s\n"
 #define DESCRIPTOR_NOT_CLOSE "Error: Can't close fd %d\n"
 /**
  * read_textfile - check the code
@@ -17,32 +17,35 @@
  */
 ssize_t cp_textfile(const char *file_from, const char *file_to)
 {
-	int fd, fd1, x;
-	char *buf = malloc(sizeof(file_from));
+	int fd, fd1;
+	ssize_t b;
+	char buf[1024];
 
 	fd = open(file_from, O_RDONLY);
-	x = read(fd, buf, 1024);
 	if (fd == -1)
 		dprintf(2, FILE_FROM_NOT_EXIST, file_from), exit(98);
-
 	fd1 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	write(fd1, buf, x);
-
-	close(fd1);
-	if (close(fd1 || fd) == -1)
-	{
-		dprintf(2, DESCRIPTOR_NOT_CLOSE, fd);
-		exit(100);
-	}
-
-	return (1);
+	if (fd1 == -1)
+		dprintf(2, FILE_TO_ERROR, file_from), exit(99);
+	while ((b = read(fd, buf, 1024)) > 0)
+		if (write(fd1, buf, b) != b)
+			dprintf(2, FILE_TO_ERROR, file_to), exit(99);
+	if (b == -1)
+		dprintf(2, FILE_FROM_NOT_EXIST, file_from), exit(98);
+	fd = close(fd);
+	fd1 = close(fd1);
+	if (fd == -1)
+		dprintf(2, DESCRIPTOR_NOT_CLOSE, fd), exit(100);
+	if (fd1 == -1)
+		dprintf(2, DESCRIPTOR_NOT_CLOSE, fd1), exit(100);
+	return (0);
 }
 int main(int ac, char **av)
 {
 	if (ac != 3)
 	{
 		dprintf(2, ARGUMENT_NOT_CORRECT);
-		exit(98);
+		exit(97);
 	}
 	cp_textfile(av[1], av[2]);
 	return (0);
