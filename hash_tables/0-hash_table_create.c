4dd93b68d596d88f8b5cf90561898215f6bd8f51
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-08 19:49:16 +0100

    hash table create

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -7,13 +7,11 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
-	unsigned long int i;
 
-	hash_table = malloc(sizeof(hash_table_t));
+	hash_table = calloc(1,sizeof(hash_table_t));
 
 	if (hash_table == NULL)
 		return (NULL);
-	return (hash_table);
 	hash_table->size = size;
 	hash_table->array = malloc(size * sizeof(hash_node_t *));
 	if (hash_table->array == NULL)
@@ -21,8 +19,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(hash_table);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
-		hash_table->array[i] = NULL;
 
 	return (hash_table);
 }
