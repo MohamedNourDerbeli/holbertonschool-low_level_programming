28f0bef4b7923b9b752f5ccf62cd0fd970c75059
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-08 19:12:48 +0100

    hash table create

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -6,7 +6,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash = malloc((size *8 +16));
+	hash_table_t *hash = malloc((size *8 +8));
 
 	if (hash == NULL)
 		return (NULL);
