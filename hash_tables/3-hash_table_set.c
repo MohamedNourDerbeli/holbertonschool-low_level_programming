ab99fd1503616038550f8f28c625d69f9f9a0c42
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-09 08:42:32 +0100

    set value

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -17,6 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	res = hash_djb2((unsigned char *)key) % ht->size;
 	node = ht->array[res];
+
 	while (node)
 	{
 		if (strcmp(node->key, key) == 0)
@@ -29,18 +30,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		node = node->next;
 	}
-	printf("pass")
 	now_node = malloc(sizeof(hash_node_t));
 	if (!now_node)
 		return (0);
 	now_node->key = strdup(key);
-	if (!node->key)
+	if (!now_node->key)
 	{
 		free(now_node);
 		return (0);
 	}
 	now_node->value = strdup(value);
-	if (!node->value)
+	if (!now_node->value)
 	{
 		free(now_node->key);
 		free(now_node);
