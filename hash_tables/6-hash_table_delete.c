4318cc2276e5d97089a7f224ff85961a85d80be8
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-09 16:24:34 +0100

    delete table

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - check the code
+ * @ht: table
+ * Return: Always EXIT_SUCCESS.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *node, *temp;
+
+
+	if (!ht)
+		return;
+
+	while (i < ht->size)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			if (node->next == NULL)
+			{
+				free(node->value);
+				free(node);
+				break;
+			}
+			
+		}
+		i++;
+	}
+}
